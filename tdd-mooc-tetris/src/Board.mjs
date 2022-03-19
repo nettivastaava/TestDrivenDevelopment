32b0a40db85b8032ac9d5d61f1262e2c8b76671e
Valtteri Andström <valtteri.andstrom@gmail.com>
2022-03-19 15:04:23 +0200

    first tetris tests passing

--- tdd-mooc-tetris/src/Board.mjs
+++ tdd-mooc-tetris/src/Board.mjs
@@ -1,13 +1,55 @@
 export class Board {
   width;
   height;
+  board;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
+
+    this.board = new Array(this.height)
+
+    for (var i=0;i<this.height;i++) {
+      this.board[i]=new Array();
+      for (var j=0;j<this.width;j++) {
+       this.board[i][j]='.';
+      }
+     }
   }
 
   toString() {
-    return "TODO";
+    var boardRepresentation = '';
+
+    for (var i=0;i<this.height;i++) {
+      for (var j=0;j<this.width;j++) {
+       boardRepresentation += this.board[i][j];
+      }
+      boardRepresentation += '\n';
+     }
+
+    return boardRepresentation;
+  }
+
+  drop() {
+    for (var i=0;i<this.height;i++) {
+      for (var j=0;j<this.width;j++) {
+        if (this.board[i][j]!=='.') {
+          throw 'already falling';
+        }
+      }
+    }
+
+    this.board[0][1] = 'X';
+  }
+
+  tick() {
+    console.log('TICK');
+    for (var i=0;i<this.height-1;i++) {
+      if (this.board[i][1] === 'X') {
+        this.board[i][1]='.';
+        this.board[i+1][1]='X';
+        break;
+      }
+    }
   }
 }
