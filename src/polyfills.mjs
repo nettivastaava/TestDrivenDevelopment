f681ddcaa9fe2ffab98143a08ad4fd75a0fa74ad
Valtteri Andström <valtteri.andstrom@gmail.com>
2022-03-19 13:47:07 +0200

    Delete src directory

--- src/polyfills.mjs
+++
@@ -1,5 +0,0 @@
-import { Temporal, toTemporalInstant } from "@js-temporal/polyfill";
-
-// TODO: remove after the Temporal API is released and included in Node.js
-global.Temporal = Temporal;
-Date.prototype.toTemporalInstant = toTemporalInstant;
